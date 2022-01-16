be477c72c15f0ee299cd9ae197baeb6f3a93f89d
Allan Simonsen <allansimonsen1972@gmail.com>
2022-01-16 07:36:59 +0100

    Bugfix: Correct mimetype is now used in the datauri Now supporting .ico files.

--- src/Convert.ts
+++ src/Convert.ts
@@ -17,13 +17,13 @@ export abstract class Convert {
     if (selection !== undefined && !selection.isEmpty) {
       const imgSrc = editor.document.getText(selection);
       let filePath = '';
-      const uriDecoded = decodeURI(imgSrc.replace(/file:\/+/i, '')).replace(/%3A/i,':');
-      if(this.fileExists(uriDecoded)) {
+      const uriDecoded = decodeURI(imgSrc.replace(/file:\/+/i, '')).replace(/%3A/i, ':');
+      if (this.fileExists(uriDecoded)) {
         filePath = uriDecoded;
       }
       else {
         const workspaceFolder = this.getWorkspaceFolder();
-        if (this.fileExists(`${workspaceFolder}/${imgSrc}`)){
+        if (this.fileExists(`${workspaceFolder}/${imgSrc}`)) {
           filePath = `${workspaceFolder}/${imgSrc}`;
         }
       }
@@ -37,7 +37,7 @@ export abstract class Convert {
         vscode.window.showInformationMessage(`File not found: ${imgSrc}`);
       }
     }
-    else{
+    else {
       vscode.window.showInformationMessage(`Nothing is selected. Select the image src in the HTML to inline it as a data uri.`);
     }
   }
@@ -54,11 +54,30 @@ export abstract class Convert {
     const imageContent = fs.readFileSync(filename);
     const base64EncodeImage = Base64.toBase64(imageContent.toString());
     const extension = path.extname(filename)
-    return `data:image/${extension};base64,${base64EncodeImage}`;
+    const mimetype = Convert.getMimeTypeFromExtension(extension);
+    return `data:${mimetype};base64,${base64EncodeImage}`;
+  }
+
+  private static getMimeTypeFromExtension(extension: string): string {
+    switch (extension.toLowerCase()) {
+      case '.gif':
+        return 'image/gif';
+      case '.ico':
+        return 'image/vnd.microsoft.icon';
+      case '.jpg':
+      case '.jpeg':
+        return 'image/jpeg';
+      case '.png':
+        return 'image/png';
+      case '.svg':
+        return 'image/svg+xml';
+      default:
+        return '';
+    }
   }
 
   private static fileExists(filename: string): boolean {
-    try{
+    try {
       return fs.lstatSync(filename).isFile();
     } catch {
       return false;
